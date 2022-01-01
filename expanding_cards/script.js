0a24c72cd0e37075e395a9edb58d9380f736bae0
Mabweh Kwasen <imabweh@gmail.com>
2022-01-01 23:52:42 +0800

    Update code for expanding_card

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -1,1 +1,21 @@
-console.log('foo')
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('mouseover', () => {
+        expandPanel(panel);
+    });
+})
+
+function expandPanel(panel) {
+    console.log(panel);
+    removeClassFromElements(panels, 'active');
+    panel.classList.add('active');
+}
+
+function removeClassFromElements(elements, className) {
+    if (elements && elements.length > 0) {
+        elements.forEach((element) => {
+            element.classList.remove(className);
+        })
+    } 
+}
